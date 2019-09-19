4a7dc0c832fb77b5b761da2c88290fe7d1416b79
Will Diep <widiep@gmail.com>
2019-09-19 09:08:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
-# My Code here....
+def map_to_negativize(arr)
+  arr.map do |num|
+    num * -1
+  end
+end
+
+def map_to_no_change(arr)
+    arr.map do |val|
+        val
+    end
+end
+
+def map_to_double(arr)
+    arr.map do |num|
+        num * 2
+    end
+end
